4fd295afca1a7dd435ba5cfeabf354f69fe36c4e
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-11 14:19:39 +0300

    Add files via upload

---
+++ if ve else1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Veritabani")
+            {
+                MessageBox.Show("En Büyük BJK");
+            }
+            
+            else
+
+            {
+                MessageBox .Show("... ::: Başka Büyük Yok::: ...");
+            }
+        }
+    }
+}
